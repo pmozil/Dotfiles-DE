c9cec2deda89a02c29cd027ec0a788e6a8165565
Богдан <44440399+BohdanOpyr@users.noreply.github.com>
2020-11-23 09:35:49 +0000

    Create setup.sh
    Just for nobody to forget to `chmod`

---
+++ setup.sh
@@ -0,0 +1,8 @@
+cp -r bspwm/ ~/.config/
+cp -r polybar/ ~/.config/
+cp -r rofi/ ~/.config/
+cp -r sxhkd/ ~/.config/
+
+chmod +x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/sxhkd/sxhkdrc
+chmod +x .config/bspwm/Scripts/powermenu
