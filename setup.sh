d2cde4c32a3fedea377a8e758fa64fa8495a635f
Богдан <44440399+BohdanOpyr@users.noreply.github.com>
2020-11-26 13:27:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
 cp -r polybar/ rofi/ bspwm/ sxhkd/ compton.conf ~/.config/
 
-chmod +x ~/.config/bspwm/bspwmrc
-chmod +x ~/.config/sxhkd/sxhkdrc
-chmod +x ~/.config/bspwm/Scripts/powermenu
+chmod +x ~/.config/bspwm/bspwmrc ~/.config/sxhkd/sxhkdrc ~/.config/bspwm/Scripts/powermenu
